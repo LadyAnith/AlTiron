5cfac93267e8889d0feeea111f1aafaf7051f478
LadyAnith <LadyAnith@gmail.com>
2020-08-18 20:15:00 +0200

    borrador

---
+++ src/Controller/AltironController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AltironController extends AbstractController
+{
+    /**
+     * @Route("/altiron", name="altiron")
+     */
+    public function index()
+    {
+        return $this->render('altiron/index.html.twig', [
+            'controller_name' => 'AltironController',
+        ]);
+    }
+}
